61f9ae9e6ca63e9f160ba36c2d14c7656a232e86
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-10-31 17:12:23 -0500

    specest.CompRoutines: correct dry-run output size allocation to match real size
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- syncopy/specest/compRoutines.py
+++ syncopy/specest/compRoutines.py
@@ -357,7 +357,12 @@ def mtmconvol_cF(
     # Get shape of output for dry-run phase
     nChannels = dat.shape[1]
     if isinstance(toi, np.ndarray):  # `toi` is an array of time-points
-        nTime = toi.size
+        # no padding: we lose half the window on each side
+        times = np.arange(0, dat.shape[0])
+        nTime = np.ceil(len(times[soi]) / (method_kwargs["nperseg"] - method_kwargs["noverlap"])).astype(np.intp)
+        nTime -= method_kwargs["nperseg"]
+        times = np.arange(0, nTime)
+        nTime = len(times[postselect])
         stftBdry = None
         stftPad = False
     else:  # `toi` is either 'all' or a percentage
